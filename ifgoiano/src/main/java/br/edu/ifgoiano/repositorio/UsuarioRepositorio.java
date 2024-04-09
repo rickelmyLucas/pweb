243ad7194a36b40b6d6edb2d2bd1042d748dd226
2020108102540248 <2020108102540248@lab02-04.ifgoiano.local>
2024-04-09 10:45:00 -0300

    commit 09/04

--- ifgoiano/src/main/java/br/edu/ifgoiano/repositorio/UsuarioRepositorio.java
+++ ifgoiano/src/main/java/br/edu/ifgoiano/repositorio/UsuarioRepositorio.java
@@ -2,4 +2,15 @@ package br.edu.ifgoiano.repositorio;
 
 public class UsuarioRepositorio {
 
+	public UsuarioRepositorio() {
+		super();
+	}
+	
+	
+	
+	public List<Usuario> listarUsuarios() {
+		List<Usuario> lstUsuarios = new ArrayList<Usuario>();
+		
+		String sql = ""
+	}
 }
